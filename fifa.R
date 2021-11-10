9ea8b714001e8d3ca14e8c19847ca443ec0cbd1e
tejseth <tejseth@umich.edu>
2021-11-10 15:45:21 -0500

    starting the markdown

---
+++ fifa.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+fifa_22 <- read_csv(url("https://raw.githubusercontent.com/tejseth/stats-401-project/master/players_fifa22.csv"))
+names(fifa_22)
+
+fifa_small <- fifa_22 %>%
+  select(FullName, Age, Height, Weight, PhotoUrl, Overall, BestPosition, ValueEUR,
+         ShootingTotal, PassingTotal, DribblingTotal, DefendingTotal, PhysicalityTotal)
+
+colSums(is.na(fifa_small))
